c68b974db9b402bd1e5bc7621a230e224094fa7f
Jacopo <jacopo.cassandro@polimi.it>
2026-08-12 12:32:26 +0200

    Restore GitHub Pages assets configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
+const repoName = "tesi-site";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   images: {
     unoptimized: true
-  }
+  },
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`
 };
 
 export default nextConfig;
